8d7fd39d28c6d53b2aa524c5904f87d15da97fa2
Qudenaza <qudenaza@gmail.com>
2017-12-24 14:08:11 +0300

    Сделал слайдер, и список товаров

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+     var slides = document.getElementsByClassName("slide");
+     var dots = document.getElementsByClassName("slider__control");
+      
+    // if (n > slides.length) slideIndex = 1;
+    // if (n < 1) slideIndex = slides.length;
+    
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (var i= 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className+= " active";
+}
