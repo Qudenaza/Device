afacdc06d289c0d1f273d4271e5540ae49c21ec5
Qudenaza <qudenaza@gmail.com>
2017-12-25 13:20:35 +0300

    Добавил функциональности слайдеру, оформил блок услуг и партнеров

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,43 @@
-var slideIndex = 1;
-
-showSlides(slideIndex);
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
-     var slides = document.getElementsByClassName("slide");
-     var dots = document.getElementsByClassName("slider__control");
-      
-    // if (n > slides.length) slideIndex = 1;
-    // if (n < 1) slideIndex = slides.length;
-    
+    var slides = document.getElementsByClassName("slide"),
+        dots = document.getElementsByClassName("slider__control");
+        
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
-    for (var i= 0; i < dots.length; i++) {
+    for (var i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
     }
 
-    slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className+= " active";
+    slides[n].style.display = "flex";
+    dots[n].className += " active";
+};
+
+showSlides(0);
+
+// var x = 0;
+
+// setInterval(function() {
+//     showSlides(x++);
+//     x > 2 ? x = 0 : x;
+// }, 4000)
+
+function showAdvantages(n) {
+    var advantages = document.querySelectorAll(".advantages__info"),
+        controls = document.querySelectorAll(".advantages__item");
+
+    
+    for (var i = 0; i < advantages.length; i++) {
+        advantages[i].style.display= "none";
+    }
+
+    for (var i = 0; i < controls.length; i++) {
+        controls[i].className = controls[i].className.replace("choosed", "");
+    }
+
+    advantages[n].style.display = "block";
+    controls[n].className += " choosed";
 }
+
+showAdvantages(0);
